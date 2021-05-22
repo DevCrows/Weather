f24256c92024291f9a9acee802ed32424f55f7c7
Francisco Barrios <francisco.barrios@ifyoucan.com>
2021-05-23 00:49:08 +0700

    WIP

--- Weather/Models/WeatherType.swift
+++ Weather/Models/WeatherType.swift
@@ -7,14 +7,46 @@
 
 import Foundation
 
-enum WeatherType {
-    case clearSky
-    case fewClouds
-    case scatteredClouds
-    case brokenClouds
-    case showerRain
-    case rain
-    case thunderstorm
-    case snow
-    case mist
+enum WeatherType: Int {
+    
+    case thunderstormWithLightRain = 200
+    case thunderstormWithRain = 201
+    case thunderstormWithHeavyRain = 202
+    case lightThunderstorm = 210
+    case thunderstorm = 211
+    case heavyThunderstorm = 212
+    case raggedThunderstorm = 221
+    case thunderstormWithLightDrizzle = 230
+    case thunderstormWithDrizzle = 231
+    case thunderstormWithHeavyDrizzle = 232
+    
+    case lightIntensityDrizzle = 300
+    case drizzle = 301
+    case heavyIntensityDrizzle = 302
+    case lightIntensityDrizzleRain = 310
+    case drizzleRain = 311
+    case heavyIntensityDrizzleRain = 312
+    case showerRainAndDrizzle = 313
+    case showerDrizzle = 314
+    
+    case lightRain = 500
+    case moderateRain = 501
+    case heavyIntensityRain = 502
+    case veryHeavyRain = 503
+    case extremeRain = 504
+    case freezingRain = 511
+    case lightIntensityShowerRain = 520
+    case showerRain = 521
+    case heavyIntensityShowerRain = 522
+    case raggedShowerRain = 531
+    
+    case clearSky = 800
+    
+    case fewClouds = 801
+    case scatteredClouds = 802
+    case brokenClouds = 803
+    case overcastClouds = 804
+
+    case snow = 601
+    case mist = 701
 }
