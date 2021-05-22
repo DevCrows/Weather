c5f9c81821ae153e02cf61bce2df3d6c7730e0fd
Francisco Barrios <francisco.barrios@ifyoucan.com>
2021-05-23 00:11:48 +0700

    WIP

---
+++ Weather/Models/WeatherType.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherType.swift
+//  Weather
+//
+//  Created by Francisco Barrios on 23/5/2564 BE.
+//
+
+import Foundation
+
+enum WeatherType {
+    case clearSky
+    case fewClouds
+    case scatteredClouds
+    case brokenClouds
+    case showerRain
+    case rain
+    case thunderstorm
+    case snow
+    case mist
+}
