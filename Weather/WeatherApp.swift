96d5396127f114f957c474b8f60bfb12f0163f94
Francisco Barrios <francisco.barrios@ifyoucan.com>
2021-05-22 10:37:11 +0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Francisco Barrios on 22/5/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
